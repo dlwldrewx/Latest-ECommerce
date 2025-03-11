fdb38542689245f23411d3eebe95fa05140f4a79
dlwldrewx <dlwldrewx@1utar.my>
2025-03-11 17:51:05 +0800

    fixed sign up page

--- app/api/test-db/route.js
+++ app/api/test-db/route.js
@@ -1,12 +1,9 @@
 import { dbConnect } from "@/lib/dbConnect";
-import User from "@/models/User";
 
 export async function GET() {
   try {
-    await dbConnect(); // Connect to MongoDB
-
-    const users = await User.find(); // Fetch users
-    return Response.json({ success: true, users }, { status: 200 });
+    await dbConnect();
+    return Response.json({ success: true, message: "Connected to MongoDB" }, { status: 200 });
   } catch (error) {
     return Response.json({ success: false, error: error.message }, { status: 500 });
   }
